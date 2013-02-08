eb937177c6d261861a6b3f0eacb59280c598ddb6
Sourabh <sourabh.agarwal@flipkart.com>
2013-02-09 02:55:13 +0530

    some skeleton code

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@
  * Initialize all routes
  */
 module.exports = function(app){
-	
+	require('./sms_route')(app);
+	require('./call_route')(app);
 };
---
+++ test.js
@@ -0,0 +1,17 @@
+var Twilio = require('twilio-js');
+Twilio.AccountSid = "AC4b2b6a51bc985aead8bbfee6edbfaa7e";
+Twilio.AuthToken  = "c10d8aa430d5cef0b16986b9da748ed8";
+
+//Twilio.Call.create({to: "+919945657973", from: "+12243658564", url: "http://4um7.localtunnel.com/call"}, function(err,res) {
+//	if(err){
+//		console.log(err);
+//	}else
+//		console.log('HOLY MOLY! PHONES ARE RINGING');
+//});
+//
+//Twilio.SMS.create({to: "+919945657973", from: "+12243658564", url: "http://4um7.localtunnel.com/sms", body : "Heeloo"}, function(err,res) {
+//	if(err){
+//		console.log(err);
+//	}else
+//		console.log('You have a new message!');
+//});
