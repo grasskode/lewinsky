02b9bc191b256fdb543940e502c1263b5e60dbb4
Karan Gupta <gupta.karan.11@gmail.com>
2013-02-10 06:29:09 +0530

    added actions to consolidation. modified and moved receipent consolidation

--- routes/note_route.js
+++ routes/note_route.js
@@ -7,7 +7,7 @@ function respond(res, code, message) {
 }
 
 module.exports = function(app) {
-        app.get("/:userid/notes", function(req, res) {
+        /*app.get("/:userid/notes", function(req, res) {
                 console.log("Getting notes for user "+req.params.userid);
                 s_notes.get(req.params.userid, req.query.count, null, function(err, data){
                         if(err)
@@ -15,7 +15,7 @@ module.exports = function(app) {
                         else
                           respond(res, 200, JSON.stringify(data));
                 });
-        });
+        });*/
 
         app.get("/:userid/notes/:noteid", function(req, res) {
                 console.log("Getting note "+req.params.noteid+" for user "+req.params.userid);
