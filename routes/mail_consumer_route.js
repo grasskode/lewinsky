22a6bc6f0282621c4712023ded4d14590303f67f
Karan Gupta <gupta.karan.11@gmail.com>
2013-03-05 10:58:32 +0530

    rewriting the scheduler

--- routes/mail_consumer_route.js
+++ routes/mail_consumer_route.js
@@ -1,7 +1,4 @@
-var _ = require('underscore');
-var s_parser = require("../services/parser");
-var s_notes = require("../services/notes");
-var logger = require("../utils/log_factory").create("mail_consumer");
+var logger = require("../utils/log_factory").create("mail_consumer_route");
 
 /**
  * Utility method to send a plain text http response with given code and message
@@ -13,62 +10,14 @@ function respond(res, code, message) {
 }
 
 /**
- * Utility function to create a note with given subject out of the given parsed text.
+ * Routes
  */
-function to_note(subject, parsedText) {
-  var note = {};
-  note['subject'] = subject;
-  note['body'] = parsedText['@body'];
-  note['receipents'] = (parsedText['@to'])?parsedText['@to'].join(", "):"";
-  note['date'] = (parsedText['@date'])?parsedText['@date'].join(", "):"";
-  note['repeat'] = (parsedText['@repeat'])?parsedText['@repeat'].join(", "):"";
-  note['actions'] = (parsedText['@action'])?parsedText['@action'].join(", "):"";
-  logger.debug(note);
-  return note;
-}
-
 module.exports = function(app) {
   
   app.post("/consume", function(req, res) {
     logger.info("Consuming mail :");
     logger.info(req.body);
-
-    /********************************************
-     * expected POST
-     ********************************************
-     * headers      The raw headers of the email.
-     * text         Text body of email. If not set, email did not have a text body.
-     * html         HTML body of email. If not set, email did not have an HTML body.
-     * from         Email sender, as taken from the message headers.
-     * to           Email recipient field, as taken from the message headers.
-     * cc           Email cc field, as taken from the message headers.
-     * subject      Email Subject.
-     * dkim         A JSON string containing the verification results of any dkim and domain keys signatures in the message.
-     * SPF          The results of the Sender Policy Framework verification of the message sender and receiving IP address.
-     * envelope     A JSON string containing the SMTP envelope. This will have two variables: 
-     *              to, which is an array of recipients, and from, which is the return path for the message.
-     * charsets     A JSON string containing the character sets of the fields extracted from the message.
-     * spam_score   Spam Assassin’s rating for whether or not this is spam.
-     * spam_report  Spam Assassin’s spam report.
-     * attachments  Number of attachments included in email.
-     * attachmentX  These are file upload names, where N is the total number of attachments. 
-     *              For example, if the number of attachments is 0, there will be no attachment files. 
-     *              If the number of attachments is 3, parameters attachment1, attachment2, and attachment3 
-     *              will have file uploads. TNEF files (winmail.dat) will be extracted and have any attachments posted.
-     **********************************************
-     */
-
-    var user_email = req.body.from;
-    var matches = user_email.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-    // ASSUMPTION : There cannot be more than one "from"
-    var userid = matches[0];
-    var subject = req.body.subject;
-    var text = req.body.text;
-        
-    var parsedText = s_parser.parse(text);
-    var note = to_note(subject, parsedText);
-
-    s_notes.create(userid, note, function(err, data) {
+    s_consumer.consume(req.body, function(err, data) {
         if(err)
             respond(res, 500, err);
         else
--- services/scheduler.js
+++ services/scheduler.js
@@ -1,68 +1,55 @@
 var _ = require('underscore');
-var logger = require(LIB_DIR + 'log_factory').create("scheduler");
-var notesImpl = require('./notes');
-var comm = require('./talk');
-var eventEmitter = require('../lib/emitter');
+var notes_dao = require('../dao/notes');
+var schedules_dao = require('../dao/schedules');
+var logger = require("../utils/log_factory").create("scheduler");
 
-var Scheduler = function(){
-	var ref = this;
-	eventEmitter.on('schedule', function(userId, subject){
-		ref.scheduleNote(userId, subject);
-	});
-	
-	/**
-	 * -> Search for all the notes scheduled for today (or the remaining time)
-	 * 	-> Create a cron expression
-	 * 	-> Search notes for that cron expression
-	 * -> schedule each note according to actions
-	 */
-	this.run = function(base){
-		/*
-		 * say today is 12-12-2012
-		 * For notes scheduled for today, the crons can be : 
-		 * --> [0-59] [0-23] 12 12 [0-7] 2012
-		 * --> [0-59] [0-23] * 12 [0-7] 2012
-		 */
-		var date = base ? base : new Date();
-		notesImpl.searchCron('^[-0-9,\*]* [-0-9,\*]* ' + 
-				'[-0-9,\*]*([[:<:]]' + date.getDate() + '[[:>:]])?[-0-9,\*]* [-0-9,\*]* [-0-9,\*]*' + 
-				'( [-0-9,\*]*[[:<:]]' + date.getFullYear() + '[[:>:]][-0-9,\*]*)?',
-			function(err, data){
-				if(!err){
-					notes = data;
-					scheduleJob(notes);
-				}else{
-					logger.error(err);
-				}
-			});
-	};
-	
-	this.scheduleNote = function(userId, subject){
-		notesImpl.searchSubject(userId, subject, function(err, notes){
-			if(!err){
-				scheduleJob(notes);
-			}else{
-				logger.error(err);
-			}
-		});
-	};
-	
-	var scheduleJob = function(notes){
-		logger.info(Object.keys(notes).length + " notes found for scheduling");
-		_.each(notes, function(note){
-			var userId = note.user;
-			var subject = note.subject;
-			
-			var noteMap = note.creation_epoch;
-			_.each(noteMap, function(entry){
-				var cron = entry.cron;
-				if(cron){
-					comm.schedule(userId, subject, cron);
-				}
-			});
-			
-		});
-	};
+exports.run = run;
+exports.scheduleNotes = scheduleNotes;
+exports.scheduleNote = scheduleNote;
+
+/**
+ * -> Search for all the notes scheduled for today (or the remaining time)
+ * 	-> Create a cron expression
+ * 	-> Search notes for that cron expression
+ * -> schedule each note according to actions
+ */
+var run = function(base){
+    /*
+     * say today is 12-12-2012
+     * For notes scheduled for today, the crons can be : 
+     * --> [0-59] [0-23] 12 12 [0-7] 2012
+     * --> [0-59] [0-23] * 12 [0-7] 2012
+     */
+    var date = base ? base : new Date();
+    s_notes.searchCron('^[-0-9,\*]* [-0-9,\*]* ' + 
+            '[-0-9,\*]*([[:<:]]' + date.getDate() + '[[:>:]])?[-0-9,\*]* [-0-9,\*]* [-0-9,\*]*' + 
+            '( [-0-9,\*]*[[:<:]]' + date.getFullYear() + '[[:>:]][-0-9,\*]*)?',
+        function(err, notes){
+            if(!err){
+                logger.info(Object.keys(notes).length + " notes found for scheduling");
+                scheduleNotes(notes);
+            }else{
+                logger.error(err);
+            }
+        });
 };
 
-module.exports = new Scheduler();
+var scheduleNotes = function(notes){
+    _.each(notes, function(note){
+        var userId = note.user;
+        var subject = note.subject;
+        var noteMap = note.creation_epoch;
+        _.each(noteMap, function(entry){
+            var cron = entry.cron;
+            if(cron){
+                schedules_dao.create();
+                //comm.schedule(userId, subject, cron);
+            }
+        });
+        
+    });
+};
+
+var scheduleNote = function(note){
+    scheduleNotes([note]);
+};
