1d714ce03fda70b56dba30e843f125efda40c7a6
Karan Gupta <gupta.karan.11@gmail.com>
2013-03-19 12:24:32 +0530

    added cancellation feature

--- services/consumer.js
+++ services/consumer.js
@@ -81,6 +81,14 @@ function to_note(subject, parsedText) {
   note['date'] = (parsedText['@date'])?parsedText['@date'].join(", "):"";
   note['repeat'] = (parsedText['@repeat'])?parsedText['@repeat'].join(", "):"";
   note['actions'] = (parsedText['@action'])?parsedText['@action'].join(", "):"";
+  if(parsedText['@cancel']){
+      for(var i=0; i<parsedText['@cancel'].length; i++) {
+          var cancel = parsedText['@cancel'][i];
+          if(note['actions'] != "")
+              note['actions'] += ", ";
+          note['actions'] += "-"+cancel;
+      }
+  }
   logger.debug(note);
   return note;
 }
